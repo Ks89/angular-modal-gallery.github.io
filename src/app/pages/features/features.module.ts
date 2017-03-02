63926082c2b024fc690cec366550b48289350b31
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-03-03 00:57:42 +0100

    other improvements to the doc + live demo example of the library

--- src/app/pages/features/features.module.ts
+++ src/app/pages/features/features.module.ts
@@ -5,10 +5,18 @@ import { routing } from './features.routing';
 
 import {FEATURES_COMPONENTS} from "./components";
 
+// ********************** angular-modal-gallery *****************************
+import 'hammerjs'; // Mandatory for angular-modal-gallery 3.x.x or greater (`npm i --save hammerjs @types/hammerjs`)
+import 'mousetrap'; // Mandatory for angular-modal-gallery 3.x.x or greater (`npm i --save mousetrap @types/mousetrap`)
+import { ModalGalleryModule } from 'angular-modal-gallery'; // <----------------- angular-modal-gallery library import
+// **************************************************************************
+
+
 @NgModule({
   imports: [
     CommonModule,
-    routing
+    routing,
+    ModalGalleryModule // no for root here
   ],
   declarations: [
     FEATURES_COMPONENTS
--- src/app/pages/features/gallery-array/gallery-array.component.ts
+++ src/app/pages/features/gallery-array/gallery-array.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {Component} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+
+import {Image, Action, ImageModalEvent, Description} from 'angular-modal-gallery';
+import {Observable} from 'rxjs/Observable';
+import {Subscription} from 'rxjs/Subscription';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/delay';
 
 @Component({
   selector: 'mmw-gallery-array-page',
@@ -8,6 +14,41 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class GalleryArrayComponent {
 
-  constructor(private route: ActivatedRoute) {}
+  imagesArray: Array<Image> = [
+    new Image(
+      '../../../../../assets/images/gallery/img1.jpg',
+      null, // no thumb
+      null, // no description
+      'http://www.google.com'
+    ),
+    new Image(
+      '../../../../../assets/images/gallery/img2.png', // example with a PNG image
+      null, // no thumb
+      'Description 2',
+      null // url
+    ),
+    new Image(
+      '../../../../../assets/images/gallery/img3.jpg',
+      '../../../../../assets/images/gallery/thumbs/img3.png', // example with a PNG thumb image
+      'Description 3',
+      'http://www.google.com'
+    ),
+    new Image(
+      '../../../../../assets/images/gallery/img4.jpg',
+      null, // no thumb
+      'Description 4',
+      'http://www.google.com'
+    ),
+    new Image(
+      '../../../../../assets/images/gallery/img5.jpg',
+      '../../../../../assets/images/gallery/thumbs/img5.jpg',
+      null, // no description
+      null // url
+    )
+  ];
+
+  constructor(private route: ActivatedRoute) {
+  }
+
 
 }
