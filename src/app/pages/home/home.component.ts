1fd8c2d086eb5d0d895c55e77c3fabbcf651356d
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-03-18 00:28:12 +0100

    another experiment

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -24,7 +24,7 @@
 
 import { Component } from '@angular/core';
 
-import {Image, Action, ImageModalEvent, Description} from 'angular-modal-gallery';
+import { Image, Action, ImageModalEvent, Description } from 'angular-modal-gallery';
 
 console.log(webpack.IMAGE_PATH_PREFIX);
 
@@ -38,32 +38,32 @@ export class HomeComponent {
 
   imagesArray: Array<Image> = [
     new Image(
-      '/assets/images/gallery/img1.jpg',
+      '${webpack.IMAGE_PATH_PREFIX}/assets/images/gallery/img1.jpg',
       null, // no thumb
       null, // no description
       'http://www.google.com'
     ),
     new Image(
-      '/assets/images/gallery/img2.png', // example with a PNG image
+      '${webpack.IMAGE_PATH_PREFIX}/assets/images/gallery/img2.png', // example with a PNG image
       null, // no thumb
       'Description 2',
       null // url
     ),
     new Image(
-      '/assets/images/gallery/img3.jpg',
-      '/assets/images/gallery/thumbs/img3.png', // example with a PNG thumb image
+      '${webpack.IMAGE_PATH_PREFIX}/assets/images/gallery/img3.jpg',
+      '${webpack.IMAGE_PATH_PREFIX}/assets/images/gallery/thumbs/img3.png', // example with a PNG thumb image
       'Description 3',
       'http://www.google.com'
     ),
     new Image(
-      '/assets/images/gallery/img4.jpg',
+      '${webpack.IMAGE_PATH_PREFIX}/assets/images/gallery/img4.jpg',
       null, // no thumb
       'Description 4',
       'http://www.google.com'
     ),
     new Image(
-      '/assets/images/gallery/img5.jpg',
-      '/assets/images/gallery/thumbs/img5.jpg',
+      '${webpack.IMAGE_PATH_PREFIX}/assets/images/gallery/img5.jpg',
+      '${webpack.IMAGE_PATH_PREFIX}/assets/images/gallery/thumbs/img5.jpg',
       null, // no description
       null // url
     )
