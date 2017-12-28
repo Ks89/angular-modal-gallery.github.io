5985c341276675dc081aac99046f02d5b4ae5cf0
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-12-28 15:11:19 +0100

    feat(): add CI + fix 404 page for github.io

---
+++ travisci/script.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# run debug build
+echo "npm run debug build on $TRAVIS_OS_NAME"
+npm run build:dev
+
+# clean before the real production build
+npm run clean
+
+# run production build
+echo "npm run production build on $TRAVIS_OS_NAME"
+npm run build:prod
+
+## run production build + Server Side Rendering
+#echo "npm run production build + ssr on $TRAVIS_OS_NAME"
+#npm run build:ssr
+#
+## run test
+#echo "npm run test on $TRAVIS_OS_NAME"
+#xvfb-run -a npm run test:ci
+#
+#sleep 5
+#
+## update webdriver to be able to run e2e tests
+#npm run webdriver:update
+#
+## here you must have ./dist folder with your app built thanks to: npm run build:prod
+#
+#sleep 5
+#
+#echo "npm run e2e on $TRAVIS_OS_NAME"
+#xvfb-run -a npm run e2e:ci
